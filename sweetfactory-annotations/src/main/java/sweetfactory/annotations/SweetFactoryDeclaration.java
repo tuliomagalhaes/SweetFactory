cb2ec2b368dc3708b0f225ebf440812ec1ec13a7
Túlio Henrique <tuliohdvp@gmail.com>
2019-06-17 15:39:04 -0300

    updated the JavaDoc of annotations

--- sweetfactory-annotations/src/main/java/sweetfactory/annotations/SweetFactoryDeclaration.java
+++ sweetfactory-annotations/src/main/java/sweetfactory/annotations/SweetFactoryDeclaration.java
@@ -7,10 +7,10 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Tell the SweetFactoryDeclaration Compiler to generate a implementation for the annotated class
+ * Tell the SweetFactory Compiler to generate a implementation for the annotated class
  * <pre><code>
- * {@literal @}SweetFactoryDeclaration
- * public interface FeatureActivityIntentFactory {}
+ * {@literal @}SweetFactoryDeclaration(factory = FeatureActivityFactory.class)
+ * public class FeatureActivity extends Activity {}
  * </code></pre>
  */
 @Target(TYPE)
--- sweetfactory-annotations/src/main/java/sweetfactory/annotations/SweetFactoryMethod.java
+++ sweetfactory-annotations/src/main/java/sweetfactory/annotations/SweetFactoryMethod.java
@@ -7,6 +7,13 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * This annotation is used in conjunction with @see {@literal @}sweetfactory.annotations.SweetFactoryDeclaration to tell the SweetFactory Compiler how to implement the factory
+ * <pre><code>
+ * {@literal @}SweetFactoryMethod
+ * public static Intent newIntent(Context context) { ... }
+ * </code></pre>
+ */
 @Documented
 @Target(METHOD)
 @Retention(RUNTIME)
